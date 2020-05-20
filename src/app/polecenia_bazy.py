e31f85b9571ef885acd1531d329e5606840c1f44
kpodwika8 <58849638+kpodwika8@users.noreply.github.com>
2020-05-20 14:08:42 +0200

    Add files via upload

--- src/app/polecenia_bazy.py
+++ src/app/polecenia_bazy.py
@@ -17,7 +17,8 @@ def wypisanie_produktow():
         i += 1
 
     connection.close()
-    
+
+
 # 4 ZAKTUALIZOWANIE CENY
 def aktualizacja_ceny(nazwa):
     connection = pymysql.Connect(
@@ -33,4 +34,39 @@ def aktualizacja_ceny(nazwa):
 
     cur.execute(sql, nazwa)
     connection.commit()
+    connection.close()
+
+#5 USUNIECIE PRODUKTU
+def usuniecie_produktu(nazwa):
+    connection = pymysql.Connect(
+        host='localhost',
+        user="root",
+        password="",
+        db="hurtownia2",
+    )
+
+    cur = connection.cursor()
+
+    sql = "DELETE FROM produkt WHERE nazwa_produktu LIKE %s;"
+
+    cur.execute(sql, nazwa)
+    connection.commit()
+    connection.close()
+
+#10 WYPISANIE PRACOWNIKOW
+def wypisanie_pracownikow():
+    connection = pymysql.connect(
+        host='localhost',
+        user="root",
+        password="",
+        db="hurtownia2",
+    )
+    cur = connection.cursor()
+    cur.execute(
+        "SELECT stanowisko.stanowisko, pracownik.imie, pracownik.nazwisko FROM pracownik INNER JOIN stanowisko ON pracownik.id_stanowisko = stanowisko.id_stanowisko ORDER BY stanowisko.stanowisko DESC;")
+    i = 1
+    for row in cur.fetchall():
+        print("Pracownik", i, " ", row[0], "|", row[1], "|", row[2])
+        i += 1
+
     connection.close()
